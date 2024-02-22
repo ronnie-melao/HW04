56b19ea8d5a6f53148ed1a495a0322a4dc7b47ad
Ronnie Melao <ronniemelao@yahoo.com>
2024-02-22 16:16:40 -0500

    changed testing procedures

--- Testing.py
+++ Testing.py
@@ -1,11 +1,11 @@
 import unittest
 
-from HW04a import getUserRepoCommits
+from HW04a.py import getUserRepoCommits
 
 class TestHW04a(unittest.TestCase):
 
     def testMyRepo(self): 
-        self.assertNotEqual(getUserRepoCommits('ronnie-melao'),'Repo: CS-546 Number of commits: 30\nRepo: HW00-Tools_Setup Number of commits: 1\nRepo: HW01-Testing-triangle-classification Number of commits: 1\nRepo: HW02 Number of commits: 13\nRepo: HW04 Number of commits: 2','Wrong commits')
+        self.assertMultiLineEqual(getUserRepoCommits('ronnie-melao'),'Repo: CS-546 Number of commits: 30\nRepo: HW00-Tools_Setup Number of commits: 1\nRepo: HW01-Testing-triangle-classification Number of commits: 1\nRepo: HW02 Number of commits: 13\nRepo: HW04 Number of commits: 2','Wrong commits')
         
     def testRichKempinski(self): 
         self.assertNotEqual(getUserRepoCommits('richkempinski'),'Repo: csp Number of commits: 2\nRepo: hellogitworld Number of commits: 30\nRepo: helloworld Number of commits: 6\nRepo: Mocks Number of commits: 10\nRepo: Project1 Number of commits: 2\nRepo: richkempinski.github.io Number of commits: 9\nRepo: threads-of-life Number of commits: 1\nRepo: try_nbdev Number of commits: \nRepo: try_nbdev2 Number of commits: 5','Wrong commits')

c69ca7da58a68f7c71a5d88f66daa64dda699fd3
Ronnie Melao <ronniemelao@yahoo.com>
2024-02-22 12:41:15 -0500

    finalized coding and testing

--- HW04a.py
+++ HW04a.py
@@ -2,12 +2,16 @@ import requests
 import modules
 
 def getUserRepoCommits(username):
+    # get url using username input
     repos_url = f"https://api.github.com/users/{username}/repos"
 
+    # use API to transform repo data in JSON file
     user_data = requests.get(repos_url).json()
-
+    print(user_data)
+    # empty array to store repo names and number of commits
     repo_info = []
 
+    # finds repo names and number of commits, then adds to empty array above
     for repo in user_data:
         repo_name = repo['name']
         commits_url = f"https://api.github.com/repos/{username}/{repo_name}/commits"
@@ -15,6 +19,7 @@ def getUserRepoCommits(username):
         num_commits = len(commits_data)
         repo_info.append(f"Repo: {repo_name} Number of commits: {num_commits}")
 
+    # joins all strings in array and makes new line after each one
     return '\n'.join(repo_info)
 
 print(getUserRepoCommits('ronnie-melao'))
